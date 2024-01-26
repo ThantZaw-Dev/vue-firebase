b4e96d6d6acc146e3cd8fa8d981971a2bfa8cc2e
X_x <bk5509404@gmail.com>
2024-01-26 12:21:46 +0630

    Firebase Testing

---
+++ firebaseConfig.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyB38l8PaupSziKPNE-zpUHaLlFA9uUB3Tk",
+    authDomain: "vuefs-project-efdde.firebaseapp.com",
+    projectId: "vuefs-project-efdde",
+    storageBucket: "vuefs-project-efdde.appspot.com",
+    messagingSenderId: "32007855416",
+    appId: "1:32007855416:web:3b2258a775d68a4760f5cd",
+    measurementId: "G-0J0S0H4QDF"
+  };
+  
